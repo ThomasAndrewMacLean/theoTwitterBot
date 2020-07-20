93109d5cac581f113ad94fafb10bbca0a545625f
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-07-20 08:59:22 +0200

    send covid data

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const Twit = require('twit');
 const AWS = require('aws-sdk');
 AWS.config.update({ region: 'eu-west-1' });
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const fetch = require('node-fetch');
 
 if (process.env.NODE_ENV === undefined) {
     require('dotenv-json')();
 }
 
-const getRandom = arr => {
+const getRandom = (arr) => {
     const randomIndex = Math.floor(Math.random() * arr.length);
 
     return arr[randomIndex];
@@ -19,11 +20,12 @@ const generateTweet = () => {
     return dynamoDb
         .scan({ TableName: 'theotwitterbot' })
         .promise()
-        .then(response => {
-            const words = response.Items.find(x => x.id === 'words').data;
-            const people = response.Items.find(x => x.id === 'people').data;
-            const hashtags = response.Items.find(x => x.id === 'hashtags').data;
-            const emojis = response.Items.find(x => x.id === 'emojis').data;
+        .then((response) => {
+            const words = response.Items.find((x) => x.id === 'words').data;
+            const people = response.Items.find((x) => x.id === 'people').data;
+            const hashtags = response.Items.find((x) => x.id === 'hashtags')
+                .data;
+            const emojis = response.Items.find((x) => x.id === 'emojis').data;
             let newTweet = '';
 
             for (let index = 0; index < 15; index++) {
@@ -37,12 +39,57 @@ const generateTweet = () => {
         });
 };
 
+const getCovidData = () => {
+    return fetch(
+        'https://europe-west1-thomasmaclean.cloudfunctions.net/getRecentCovidData'
+    )
+        .then((x) => x.json())
+        .then((y) => {
+            const totaalAantal = y.totaal
+                .trim()
+                .split('*')
+                .join('')
+                .split(' ')
+                .filter((x, i) => i < 2)
+                .join(' ');
+
+            const totaalPerc = y.totaal.trim().split(' ').pop();
+
+            const sterfAantal = y.sterfGevallen
+                .trim()
+                .split('*')
+                .join('')
+                .split(' ')
+                .filter((x, i) => i < 2)
+                .join(' ');
+
+            const sterfPerc = y.sterfGevallen.trim().split(' ').pop();
+
+            const opnameAantal = y.opnames
+                .trim()
+                .split('*')
+                .join('')
+                .split(' ')
+                .filter((x, i) => i < 2)
+                .join(' ');
+
+            const opnamePerc = y.opnames.trim().split(' ').pop();
+
+            let newTweet = 'COVID DATA: ' + y.datum + '\n' + '\n' 
+                + `Bevestigde gevallen: ${totaalAantal} (${totaalPerc})\n`
+                + `Sterfgevallen: ${sterfAantal} (${sterfPerc})\n`
+                + `Opnames in het ziekenhuis: ${opnameAantal} (${opnamePerc})\n`
+            ;
+            return newTweet
+        });
+};
+
 const getHelfies = () => {
     return dynamoDb
         .scan({ TableName: 'theotwitterbot' })
         .promise()
-        .then(response => {
-            const helfies = response.Items.filter(x => x.type === 'helfie');
+        .then((response) => {
+            const helfies = response.Items.filter((x) => x.type === 'helfie');
 
             return helfies;
         });
@@ -52,23 +99,23 @@ const T = new Twit({
     consumer_secret: process.env.CONS_SECRET,
     access_token: process.env.ACCESS_TOKEN,
     access_token_secret: process.env.ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000
+    timeout_ms: 60 * 1000,
 });
 
-api.get('/helfies', request => {
+api.get('/helfies', (request) => {
     // GET all data
     return getHelfies();
 });
 
-api.get('/database', request => {
+api.get('/database', (request) => {
     // GET all data
     return dynamoDb
         .scan({ TableName: 'theotwitterbot' })
         .promise()
-        .then(response => response);
+        .then((response) => response);
 });
 
-api.get('/randomTweet', request => {
+api.get('/randomTweet', (request) => {
     return generateTweet();
 });
 
@@ -91,7 +138,39 @@ api.put('/tweet', (req, res) => {
         );
     }
 
-    return generateTweet().then(tweet => {
+    return generateTweet().then((tweet) => {
+        T.post('statuses/update', { status: tweet }, (err, data, response) => {
+            if (err)
+                throw new ApiBuilder.ApiResponse(
+                    '<error>' + err + '</error>',
+                    { 'Content-Type': 'text/xml' },
+                    500
+                );
+            return data;
+        });
+    });
+});
+
+api.put('/covid', (req, res) => {
+    const b64auth = (req.headers.Authorization || '').split(' ')[1] || '';
+    const [login, password] = new Buffer(b64auth, 'base64')
+        .toString()
+        .split(':');
+
+    if (
+        !login ||
+        !password ||
+        login !== 'theobot' ||
+        password !== process.env.CRON_SECRET
+    ) {
+        throw new ApiBuilder.ApiResponse(
+            '<error>' + login + ' login error</error>',
+            { 'Content-Type': 'text/xml' },
+            500
+        );
+    }
+
+    return getCovidData().then((tweet) => {
         T.post('statuses/update', { status: tweet }, (err, data, response) => {
             if (err)
                 throw new ApiBuilder.ApiResponse(
@@ -105,14 +184,14 @@ api.put('/tweet', (req, res) => {
 });
 api.post(
     '/addhelfie',
-    function(request) {
+    function (request) {
         var params = {
             TableName: 'theotwitterbot',
             Item: {
                 id: 'helfie:' + request.body.helfie,
                 name: request.body.helfie,
-                type: 'helfie'
-            }
+                type: 'helfie',
+            },
         };
         return dynamoDb.put(params).promise();
     },
@@ -121,18 +200,18 @@ api.post(
 
 api.post(
     '/updatehelfie',
-    async request => {
+    async (request) => {
         const helfie = request.body.helfie;
         const helfies = await getHelfies();
 
-        const hh = helfies.find(h => h.name === helfie);
+        const hh = helfies.find((h) => h.name === helfie);
         if (hh) {
             var params = {
                 TableName: 'theotwitterbot',
                 Item: {
                     ...hh,
-                    laatstetweet: 'haha' + Date.now().toString()
-                }
+                    laatstetweet: 'haha' + Date.now().toString(),
+                },
             };
 
             return dynamoDb.put(params).promise();
@@ -170,7 +249,7 @@ api.put('/answerhelfies', async (req, res) => {
         'Uhu',
         'Zo had ik het nog niet bekeken',
         'Ja',
-        'Inderdaad'
+        'Inderdaad',
     ];
 
     const openingszin = [
@@ -198,33 +277,33 @@ api.put('/answerhelfies', async (req, res) => {
         'Mag ik twee bier, een cola, en jouw telefoonnummer?',
         'Er moet iets aan de hand zijn met mijn ogen want ik kan ze niet van je afhouden.',
         'Ik wil met jou wel eens een beschuitje eten.',
-        'Ik wil met jou wel eens een potje klaverjassen.'
+        'Ik wil met jou wel eens een potje klaverjassen.',
     ];
 
     const helfies = await getHelfies();
 
     return Promise.all(
-        helfies.map(async h => {
+        helfies.map(async (h) => {
             const helfie = '@' + h.name + ' ';
             console.log(helfie);
             return await T.get('statuses/user_timeline', {
                 screen_name: helfie,
                 count: 10,
-                tweet_mode: 'extended'
+                tweet_mode: 'extended',
             })
-                .then(data => {
+                .then((data) => {
                     console.log('komt ieee');
                     const ownTweets = data.data
                         .filter(
-                            tw =>
+                            (tw) =>
                                 // remove reply tweets
                                 tw.in_reply_to_status_id === null &&
                                 // remove retweets
                                 tw.retweeted_status === undefined
                         )
-                        .map(tweet => ({
+                        .map((tweet) => ({
                             text: tweet.full_text,
-                            id: tweet.id_str
+                            id: tweet.id_str,
                         }));
                     if (
                         ownTweets.length &&
@@ -239,9 +318,9 @@ api.put('/answerhelfies', async (req, res) => {
                                     getRandom(compliment) +
                                     ',... ' +
                                     getRandom(openingszin),
-                                in_reply_to_status_id: ownTweets[0].id
+                                in_reply_to_status_id: ownTweets[0].id,
                             },
-                            function(err, data, response) {
+                            function (err, data, response) {
                                 if (err) {
                                     console.log(err);
                                 } else {
@@ -249,8 +328,8 @@ api.put('/answerhelfies', async (req, res) => {
                                         TableName: 'theotwitterbot',
                                         Item: {
                                             ...h,
-                                            laatstetweet: ownTweets[0].id
-                                        }
+                                            laatstetweet: ownTweets[0].id,
+                                        },
                                     };
 
                                     return dynamoDb.put(params).promise();
@@ -260,13 +339,13 @@ api.put('/answerhelfies', async (req, res) => {
                         );
                     }
                 })
-                .catch(err => {
+                .catch((err) => {
                     var params = {
                         TableName: 'theotwitterbot',
                         Item: {
                             ...h,
-                            error: err.message
-                        }
+                            error: err.message,
+                        },
                     };
 
                     return dynamoDb.put(params).promise();
@@ -301,7 +380,7 @@ api.put('/answer', (req, res) => {
         'Wat een inzicht',
         'Inderdaad',
         'Dacht ik ook',
-        'Geniaal ben jij'
+        'Geniaal ben jij',
     ];
 
     const answers = [
@@ -311,7 +390,7 @@ api.put('/answer', (req, res) => {
         'Je lijkt heel erg op mijn toekomstige vrouw..',
         'Is het nou zo warm hier binnen of ben jij dat?',
         'Deed het pijn, Toen je uit de hemel kwam vallen?',
-        'Mijn hobby is puzzelen en jij bent het laatste stukje dat ik zocht.'
+        'Mijn hobby is puzzelen en jij bent het laatste stukje dat ik zocht.',
     ];
 
     return T.get(
@@ -319,20 +398,20 @@ api.put('/answer', (req, res) => {
         {
             screen_name: helfie,
             count: 10,
-            tweet_mode: 'extended'
+            tweet_mode: 'extended',
         },
-        function(err, data, response) {
+        function (err, data, response) {
             const ownTweets = data
                 .filter(
-                    tw =>
+                    (tw) =>
                         // remove reply tweets
                         tw.in_reply_to_status_id === null &&
                         // remove retweets
                         tw.retweeted_status === undefined
                 )
-                .map(tweet => ({
+                .map((tweet) => ({
                     text: tweet.full_text,
-                    id: tweet.id_str
+                    id: tweet.id_str,
                 }));
             T.post(
                 'statuses/update',
@@ -342,9 +421,9 @@ api.put('/answer', (req, res) => {
                         getRandom(compliment) +
                         ',... ' +
                         getRandom(answers),
-                    in_reply_to_status_id: ownTweets[0].id
+                    in_reply_to_status_id: ownTweets[0].id,
                 },
-                function(err, data, response) {
+                function (err, data, response) {
                     if (err) {
                         console.log(err);
                     } else {
